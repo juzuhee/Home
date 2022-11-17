8c290967164598fa5e07082b5e9c3b8b2406f291
ChoiSuJin03 <106504517+ChoiSuJin03@users.noreply.github.com>
2022-11-17 11:17:10 +0900

    test

---
+++ php.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>로그인</title>
+        <meta charset="utf-8">
+        <link rel = "stylesheet" href="css/php.css">
+        <link href="https://fonts.googleapis.com/earlyaccess/notosanskr.css" rel="stylesheet">
+    </head>
+    <body>
+        <h3>LOGIN</h3>
+        <div class="login-form">
+            <form>
+                <input type="text" name="email" class="text-field" placeholder="ID"></br>
+                <input type="text" name="password" class="text-field" placeholder="PASSWORD"></br>
+                <input type="submit" value="로그인" class="submit-btn">
+            </form>
+
+            <div class="links">
+                <a href="#">비밀번호 잊음?ㅋㅋ</a>
+            </div>
+        </div>
+    </body>
+</html>
+
+
+
+
+<?php
+ echo("hi")
+?>
